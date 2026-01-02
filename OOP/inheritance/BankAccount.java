ff0a67f19130b95bebcca95cdb94d6f4283685e7
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-02 20:13:05 +0530

    Added parent class 'Account' with encapsulation

---
+++ OOP/inheritance/BankAccount.java
@@ -0,0 +1,23 @@
+class Account {
+    private String accNo;
+    private String name;
+    private String address;
+    private String PhnNo;
+    private String DOB;
+    private double balance;
+
+    Account(String accNo, String name, String address, String PhnNo, String DOB, double balance) {
+        this.accNo = accNo;
+        this.name = name;
+        this.address = address;
+        this.PhnNo = PhnNo;
+        this.DOB = DOB;
+        this.balance = balance;
+    }
+}
+
+public class BankAccount {
+    public static void main(String[] args) {
+        
+    }    
+}
