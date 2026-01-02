69ce0a7c634bebd16f8b260e2c7a85e9ead971f5
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-02 22:21:52 +0530

    Added Account class constructor and private fields

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,19 @@
 class Account {
-   
+    private String accNo;
+    private String name;
+    private String address;
+    private String phnNo;
+    private String dob;
+    private double balance;
+
+    Account(String accNo, String name, String address, String phnNo, String dob, double balance) {
+        this.accNo = accNo;
+        this.name = name;
+        this.address = address;
+        this.phnNo = phnNo;
+        this.dob = dob;
+        this.balance = balance;
+    }
 }
 
 class SavingsAccount extends Account {
