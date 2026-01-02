8a7e913ffcb3e808442d9a5b945e22a842094117
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-02 15:45:54 +0530

    Added gitignore to ignore class files

---
+++ OOP/inheritance/Overriding.java
@@ -0,0 +1,25 @@
+package OOP.inheritance;
+
+class Car {
+    public void start() {
+        System.out.println("Car has started.");
+    }
+
+    public void stop() {
+        System.out.println("Car has stopped.");
+    }
+
+    public void AC() {
+        System.out.println("AC is turned on.");
+    }
+
+    public void changeGear() {
+        System.out.println("Gear has been changed.");
+    }
+}
+
+public class Overriding {
+    public static void main(String[] args) {
+
+    }
+}
