de4e316c3f262174dbe9dd439d3082fbcd9ef0bf
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-02 11:50:06 +0530

    Created basic class structure of Rectangle and Cuboid

---
+++ OOP/inheritance/InheritanceRectangle.java
@@ -0,0 +1,16 @@
+package OOP.inheritance;
+
+class Rectangle {
+    double length;
+    double breadth;
+}
+
+class Cuboid extends Rectangle {
+    double height;
+}
+
+public class InheritanceRectangle {
+    public static void main(String[] args) {
+
+    }
+}
