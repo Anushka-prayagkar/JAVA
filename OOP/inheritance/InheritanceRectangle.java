006603909c3b324b2917a9742356797951ea5805
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-02 11:53:44 +0530

    Added constructors to Rectangle class

--- OOP/inheritance/InheritanceRectangle.java
+++ OOP/inheritance/InheritanceRectangle.java
@@ -3,6 +3,14 @@ package OOP.inheritance;
 class Rectangle {
     double length;
     double breadth;
+
+    Rectangle() {
+        length = breadth = 1;
+    }
+    Rectangle(double l, double b) {
+        length = l;
+        breadth = b;
+    }
 }
 
 class Cuboid extends Rectangle {
