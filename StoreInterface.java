3bf2e5aee5711f165f12cf370f266b5e89b34868
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-03 22:11:58 +0530

    Added array which can hold 100 members, and counter to keep track of customers

--- StoreInterface.java
+++ StoreInterface.java
@@ -3,7 +3,18 @@ interface iMember {
 }
 
 class Store {
+    iMember members [] = new iMember[100];
+    int counter = 0;
+    
+    void register(iMember m) {
+        members[counter++] = m;
+    }
 
+    void inviteSale() {
+        for(int i = 0; i < counter; i++) {
+            members[i].callback();
+        }
+    }
 }
 
 class Customer implements iMember {
