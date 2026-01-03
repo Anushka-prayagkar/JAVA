3c85b9c3ec72e4741943541d1caca551834105ac
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-03 16:25:01 +0530

    Added basic structure of abstract class, child classes and main class

---
+++ Shape.java
@@ -0,0 +1,17 @@
+abstract class Shapes {
+
+}
+
+class Circle extends Shapes {
+
+}
+
+class Rectangle extends Shapes {
+
+}
+
+public class Shape {
+    public static void main(String[] args) {
+        
+    }
+}
