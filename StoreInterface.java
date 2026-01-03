317efdbff5996865d267d99bb86ce66ae2222adc
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-03 22:06:07 +0530

    Created basic class structure

---
+++ StoreInterface.java
@@ -0,0 +1,17 @@
+interface iMember {
+
+}
+
+class Store {
+
+}
+
+class Customer implements iMember {
+
+}
+
+public class StoreInterface {
+    public static void main(String[] args) {
+    
+    }
+}
