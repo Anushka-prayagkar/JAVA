3d86b6c4201ba1c504da526a4502baf0e83ec32c
Anushka-prayagkar <anushka.prayagkar@gmail.com>
2026-01-03 21:02:30 +0530

    Created basic structure of parent class, child class, interfaces and main class

---
+++ Interface.java
@@ -0,0 +1,22 @@
+class Phone {
+
+}
+
+interface iCamera {
+
+}
+
+interface iMusicPlayer {
+
+}
+
+class SmartPhone extends Phone implements iCamera, iMusicPlayer{
+
+}
+
+
+public class Interface {
+    public static void main(String[] args) {
+                
+    }
+}
